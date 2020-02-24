3338ccab349d64a674fa3330243f9bc4bac1c1bc
JoshuaHamann <joshwashku@yahoo.com>
2020-02-24 10:21:15 -0600

    Close issue #1 with search filter status

--- client/src/app/todos/todo.service.ts
+++ client/src/app/todos/todo.service.ts
@@ -29,7 +29,7 @@ export class TodoService {
       }
 
       if (filters.limit) {
-        httpParams = httpParams.set('limit', filters.limit.toString());
+        httpParams = httpParams.set('limit',filters.limit.toString());
       }
 
     }
